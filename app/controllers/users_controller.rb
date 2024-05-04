c1d53fec39ee5a6a583cc0f17b68ba929d7b74a3
honami727 <honami727mogul@gmail.com>
2024-05-04 15:35:38 +0000

    User一覧編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
@@ -6,6 +13,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
   end
   
 end
