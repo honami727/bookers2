e3456f73e758a0c2babe907cc4e9fa9a5ba2dbef
honami727 <honami727mogul@gmail.com>
2024-05-09 15:32:44 +0000

    エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,20 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
+    @book.user_id = current_user.id
+    @book.save!
     redirect_to '/homes/about'
   end
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
-    @users = User.all
+    @user = @book.user
   end
 
   def edit
