4314e368e5b9b2eefd018389cc32ad3c14de71f1
honami727 <honami727mogul@gmail.com>
2024-05-18 14:13:13 +0000

    レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  delete 'books/:id' => 'books#destroy',as: 'destroy_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   post 'books' => 'books#create'
+  post 'books/:id/edit' => 'books#edit', as: 'edit_book'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   root to: 'homes#top'
